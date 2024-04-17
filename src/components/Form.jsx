d5004be83c2017b2e0525cbee608f721fe62d804
Jordan Burger <jordanburger22@gmail.com>
2024-04-17 11:10:12 -0600

    init

---
+++ src/components/Form.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useContext } from 'react';
+import { Context } from '../context/ContextProvider';
+
+
+function Form() {
+
+
+
+    const [formData, setFormData] = useState({
+        title: '',
+        description: '',
+        price: '',
+    })
+
+    function handleChange(e){
+        const {name, value} = e.target
+        setFormData(prevData => {
+            return {
+                ...prevData,
+                [name]: value
+            }
+        })
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(formData)
+    }
+
+    return ( 
+        <div className='main form-container'>
+            <form onSubmit={handleSubmit} className='form'>
+            <h1 className='form-header'>Add a todo!</h1>
+                <input 
+                    name='title'
+                    value={formData.title}
+                    onChange={handleChange}
+                    placeholder='Title'                
+                />
+
+                <input 
+                    name='description'
+                    value={formData.description}
+                    onChange={handleChange}
+                    placeholder='Description'                
+                />
+
+                <input 
+                    name='price'
+                    value={formData.price}
+                    onChange={handleChange}
+                    placeholder='Price'                
+                />
+
+                <button>Submit!</button>
+            </form>
+        </div>
+     );
+}
+
+export default Form;
